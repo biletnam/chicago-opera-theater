702704ded8948f43b03051990e831a05847515ae
Joshua Bartlett <joshuabartlett@gmail.com>
2017-06-01 14:08:55 -0500

    Initial commit

---
+++ inc/jetpack.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Jetpack Compatibility File
+ *
+ * @link https://jetpack.com/
+ *
+ * @package Chicago_Opera_Theater
+ */
+
+/**
+ * Jetpack setup function.
+ *
+ * See: https://jetpack.com/support/infinite-scroll/
+ * See: https://jetpack.com/support/responsive-videos/
+ */
+function chicago_opera_theater_jetpack_setup() {
+	// Add theme support for Infinite Scroll.
+	add_theme_support( 'infinite-scroll', array(
+		'container' => 'main',
+		'render'    => 'chicago_opera_theater_infinite_scroll_render',
+		'footer'    => 'page',
+	) );
+
+	// Add theme support for Responsive Videos.
+	add_theme_support( 'jetpack-responsive-videos' );
+}
+add_action( 'after_setup_theme', 'chicago_opera_theater_jetpack_setup' );
+
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function chicago_opera_theater_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+			get_template_part( 'template-parts/content', 'search' );
+		else :
+			get_template_part( 'template-parts/content', get_post_format() );
+		endif;
+	}
+}
---
+++ readme.txt
@@ -0,0 +1,1 @@
+The theme for Chicago Opera Theater
