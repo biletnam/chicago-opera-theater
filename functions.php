6e82cae9ac53202d2eaa6bfc77297e19fea3df20
Joshua Bartlett <joshuabartlett@gmail.com>
2017-06-27 09:51:14 -0500

    Remove unecessary files

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  * @package Chicago_Opera_Theater
  */
 
-if ( ! function_exists( 'chicago_opera_theater_setup' ) ) :
+if (! function_exists('chicago_opera_theater_setup')) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
  *
@@ -15,61 +15,62 @@ if ( ! function_exists( 'chicago_opera_theater_setup' ) ) :
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function chicago_opera_theater_setup() {
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on Chicago Opera Theater, use a find and replace
-	 * to change 'chicago-opera-theater' to the name of your theme in all the template files.
-	 */
-	load_theme_textdomain( 'chicago-opera-theater', get_template_directory() . '/languages' );
+function chicago_opera_theater_setup()
+{
+    /*
+     * Make theme available for translation.
+     * Translations can be filed in the /languages/ directory.
+     * If you're building a theme based on Chicago Opera Theater, use a find and replace
+     * to change 'chicago-opera-theater' to the name of your theme in all the template files.
+     */
+    load_theme_textdomain('chicago-opera-theater', get_template_directory() . '/languages');
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+    // Add default posts and comments RSS feed links to head.
+    add_theme_support('automatic-feed-links');
 
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
+    /*
+     * Let WordPress manage the document title.
+     * By adding theme support, we declare that this theme does not use a
+     * hard-coded <title> tag in the document head, and expect WordPress to
+     * provide it for us.
+     */
+    add_theme_support('title-tag');
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-	 */
-	add_theme_support( 'post-thumbnails' );
+    /*
+     * Enable support for Post Thumbnails on posts and pages.
+     *
+     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+     */
+    add_theme_support('post-thumbnails');
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'menu-1' => esc_html__( 'Primary', 'chicago-opera-theater' ),
-	) );
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus(array(
+        'menu-1' => esc_html__('Primary', 'chicago-opera-theater'),
+    ));
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
+    /*
+     * Switch default core markup for search form, comment form, and comments
+     * to output valid HTML5.
+     */
+    add_theme_support('html5', array(
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    ));
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'chicago_opera_theater_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
+    // Set up the WordPress core custom background feature.
+    add_theme_support('custom-background', apply_filters('chicago_opera_theater_custom_background_args', array(
+        'default-color' => 'ffffff',
+        'default-image' => '',
+    )));
 
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+    // Add theme support for selective refresh for widgets.
+    add_theme_support('customize-selective-refresh-widgets');
 }
 endif;
-add_action( 'after_setup_theme', 'chicago_opera_theater_setup' );
+add_action('after_setup_theme', 'chicago_opera_theater_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -78,44 +79,30 @@ add_action( 'after_setup_theme', 'chicago_opera_theater_setup' );
  *
  * @global int $content_width
  */
-function chicago_opera_theater_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'chicago_opera_theater_content_width', 640 );
+function chicago_opera_theater_content_width()
+{
+    $GLOBALS['content_width'] = apply_filters('chicago_opera_theater_content_width', 640);
 }
-add_action( 'after_setup_theme', 'chicago_opera_theater_content_width', 0 );
+add_action('after_setup_theme', 'chicago_opera_theater_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function chicago_opera_theater_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'chicago-opera-theater' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'chicago-opera-theater' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+function chicago_opera_theater_widgets_init()
+{
+    register_sidebar(array(
+        'name'          => esc_html__('Sidebar', 'chicago-opera-theater'),
+        'id'            => 'sidebar-1',
+        'description'   => esc_html__('Add widgets here.', 'chicago-opera-theater'),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ));
 }
-add_action( 'widgets_init', 'chicago_opera_theater_widgets_init' );
-
-/**
- * Enqueue scripts and styles.
- */
-function chicago_opera_theater_scripts() {
-	wp_enqueue_style( 'chicago-opera-theater-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'chicago-opera-theater-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'chicago-opera-theater-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'chicago_opera_theater_scripts' );
+add_action('widgets_init', 'chicago_opera_theater_widgets_init');
 
 /**
  * Implement the Custom Header feature.
@@ -131,13 +118,3 @@ require get_template_directory() . '/inc/template-tags.php';
  * Custom functions that act independently of the theme templates.
  */
 require get_template_directory() . '/inc/extras.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-require get_template_directory() . '/inc/jetpack.php';
