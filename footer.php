d5e09ee2e164756035a6eb5ee1dd00135fc964c6
Joshua Bartlett <joshuabartlett@gmail.com>
2017-06-26 21:15:58 -0500

    Add resource routes

--- footer.php
+++ footer.php
@@ -15,13 +15,14 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'chicago-opera-theater' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'chicago-opera-theater' ), 'WordPress' ); ?></a>
+			<a href="<?php echo esc_url(__('https://wordpress.org/', 'chicago-opera-theater')); ?>"><?php printf(esc_html__('Proudly powered by %s', 'chicago-opera-theater'), 'WordPress'); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'chicago-opera-theater' ), 'chicago-opera-theater', '<a href="https://automattic.com/" rel="designer">Joshua Bartlett</a>' ); ?>
+			<?php printf(esc_html__('Theme: %1$s by %2$s.', 'chicago-opera-theater'), 'chicago-opera-theater', '<a href="https://automattic.com/" rel="designer">Joshua Bartlett</a>'); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/javascript/script.js"></script>
 <?php wp_footer(); ?>
 
 </body>
--- header.php
+++ header.php
@@ -12,37 +12,37 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-
+<link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet"/>
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'chicago-opera-theater' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'chicago-opera-theater'); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+            if (is_front_page() && is_home()) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
 			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
 			<?php
-			endif;
+            endif;
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
+            $description = get_bloginfo('description', 'display');
+            if ($description || is_customize_preview()) : ?>
 				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
 			<?php
-			endif; ?>
+            endif; ?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'chicago-opera-theater' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'chicago-opera-theater'); ?></button>
+			<?php wp_nav_menu(array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' )); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
