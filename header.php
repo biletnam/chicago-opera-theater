08391e613c958c1f1e768ed5db0dc7aa19adaff2
Joshua Bartlett <joshuabartlett@gmail.com>
2017-06-28 09:19:20 -0500

    Add material design icons

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 <meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
 <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet"/>
 <?php wp_head(); ?>
 </head>
