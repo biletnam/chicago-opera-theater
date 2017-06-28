4cbfb1106a52b318b878d164e82812e7d62e0c0c
Joshua Bartlett <joshuabartlett@gmail.com>
2017-06-27 19:12:01 -0500

    Remove unnecessary code

--- footer.php
+++ footer.php
@@ -11,14 +11,13 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url(__('https://wordpress.org/', 'chicago-opera-theater')); ?>"><?php printf(esc_html__('Proudly powered by %s', 'chicago-opera-theater'), 'WordPress'); ?></a>
-			<span class="sep"> | </span>
-			<?php printf(esc_html__('Theme: %1$s by %2$s.', 'chicago-opera-theater'), 'chicago-opera-theater', '<a href="https://automattic.com/" rel="designer">Joshua Bartlett</a>'); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="site-info">
+		<a href="<?php echo esc_url(__('https://wordpress.org/', 'chicago-opera-theater')); ?>"><?php printf(esc_html__('Proudly powered by %s', 'chicago-opera-theater'), 'WordPress'); ?></a>
+		<span class="sep"> | </span>
+		<?php printf(esc_html__('Theme: %1$s by %2$s.', 'chicago-opera-theater'), 'chicago-opera-theater', '<a href="https://automattic.com/" rel="designer">Joshua Bartlett</a>'); ?>
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/javascript/script.js"></script>
 <?php wp_footer(); ?>
