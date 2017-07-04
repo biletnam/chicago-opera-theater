52fd10da4d7df6e21c32a11d1bf7dad1bdb2db35
Joshua Bartlett <joshuabartlett@gmail.com>
2017-07-04 08:28:17 -0500

    Remove unecessary functions

--- functions.php
+++ functions.php
@@ -18,17 +18,6 @@ if (! function_exists('chicago_opera_theater_setup')) :
 function chicago_opera_theater_setup()
 {
     /*
-     * Make theme available for translation.
-     * Translations can be filed in the /languages/ directory.
-     * If you're building a theme based on Chicago Opera Theater, use a find and replace
-     * to change 'chicago-opera-theater' to the name of your theme in all the template files.
-     */
-    load_theme_textdomain('chicago-opera-theater', get_template_directory() . '/languages');
-
-    // Add default posts and comments RSS feed links to head.
-    add_theme_support('automatic-feed-links');
-
-    /*
      * Let WordPress manage the document title.
      * By adding theme support, we declare that this theme does not use a
      * hard-coded <title> tag in the document head, and expect WordPress to
@@ -42,29 +31,9 @@ function chicago_opera_theater_setup()
      * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
      */
     add_theme_support('post-thumbnails');
-
-    /*
-     * Switch default core markup for search form, comment form, and comments
-     * to output valid HTML5.
-     */
-    add_theme_support('html5', array(
-        'search-form',
-        'comment-form',
-        'comment-list',
-        'gallery',
-        'caption',
-    ));
-
-    // Set up the WordPress core custom background feature.
-    add_theme_support('custom-background', apply_filters('chicago_opera_theater_custom_background_args', array(
-        'default-color' => 'ffffff',
-        'default-image' => '',
-    )));
-
-    // Add theme support for selective refresh for widgets.
-    add_theme_support('customize-selective-refresh-widgets');
 }
 endif;
+
 add_action('after_setup_theme', 'chicago_opera_theater_setup');
 
 /**
