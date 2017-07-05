a630beec6d74aed457cdbd3d94e7afd514565b84
Joshua Bartlett <joshuabartlett@gmail.com>
2017-07-05 14:46:02 -0500

    Add override to Bulma shortcodes

--- functions.php
+++ functions.php
@@ -17,20 +17,20 @@ if (! function_exists('chicago_opera_theater_setup')) :
  */
 function chicago_opera_theater_setup()
 {
-    /*
-     * Let WordPress manage the document title.
-     * By adding theme support, we declare that this theme does not use a
-     * hard-coded <title> tag in the document head, and expect WordPress to
-     * provide it for us.
-     */
-    add_theme_support('title-tag');
+		/*
+																																																																 * Let WordPress manage the document title.
+																																																																 * By adding theme support, we declare that this theme does not use a
+																																																																 * hard-coded <title> tag in the document head, and expect WordPress to
+																																																																 * provide it for us.
+																																																																 */
+																																																																add_theme_support('title-tag');
 
-    /*
-     * Enable support for Post Thumbnails on posts and pages.
-     *
-     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-     */
-    add_theme_support('post-thumbnails');
+																																																																/*
+																																																																 * Enable support for Post Thumbnails on posts and pages.
+																																																																 *
+																																																																 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+																																																																 */
+																																																																add_theme_support('post-thumbnails');
 }
 endif;
 
@@ -51,7 +51,7 @@ add_filter('tablepress_use_default_css', '__return_false');
  */
 function chicago_opera_theater_content_width()
 {
-    $GLOBALS['content_width'] = apply_filters('chicago_opera_theater_content_width', 640);
+		$GLOBALS['content_width'] = apply_filters('chicago_opera_theater_content_width', 640);
 }
 add_action('after_setup_theme', 'chicago_opera_theater_content_width', 0);
 
@@ -95,6 +95,38 @@ require get_template_directory() . '/inc/remove-image-hardcodes.php';
  */
 function my_acf_init()
 {
-    acf_update_setting('google_api_key', 'AIzaSyCGdjjfkBYuZk3gcYd1ECkkX9NO91S4lWw');
+		acf_update_setting('google_api_key', 'AIzaSyCGdjjfkBYuZk3gcYd1ECkkX9NO91S4lWw');
 }
 add_action('acf/init', 'my_acf_init');
+
+/**
+ * Overrides a shortcode from the Bulma Shortcodes plugin
+ */
+function custom_bulma_sCardFooter($atts, $content = null)
+{
+		$parsed_content = do_shortcode($content);
+		extract(shortcode_atts(array(), $atts));
+		if (strpos($parsed_content, '<a') !== false) {
+				$atemplate = "
+			 <a href='$1' class='has-arrow-icon'>
+				 $2
+				 <span class='icon'>
+					 <i class='fa fa-arrow-right'></i>
+				 </span>
+			 </a>";
+
+				$parsed_content = preg_replace('/<a href=\"(.*?)\">(.*?)<\/a>/', $atemplate, $parsed_content);
+		}
+
+		$footer_item_array = bulma_split_at_bar($parsed_content);
+		$concatenated_content = '';
+
+		foreach ($footer_item_array as $item) {
+				$string = '<div class="card-footer-item">'.$item.'</div>';
+				$concatenated_content .= $string;
+		}
+
+		return '<footer class="card-footer">'.$concatenated_content.'</footer>';
+}
+
+add_shortcode('bulma-card-footer', 'custom_bulma_sCardFooter');
