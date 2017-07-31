b0b9816dbc21eac67c584ffd563a90fe25ed664a
Joshua Bartlett <joshuabartlett@gmail.com>
2017-07-31 12:00:37 -0500

    Updates to email

--- inc/send-subscription-notification-email.php
+++ inc/send-subscription-notification-email.php
@@ -24,22 +24,20 @@ function send_cot_subscription_notification($params, $refID) {
   $message .= "\n";
   $message .= "==========================================\n";
   $message .= "\n";
-  $message .= 'Subscription holder: ' . $params['customer']['subscriber_salutation'] . ' ' . $params['customer']['subscriber_name'] . "\n";
+  $message .= 'Subscription Name: ' . $params['customer']['subscriber_salutation'] . ' ' . $params['customer']['subscriber_name'] . "\n";
   $message .= 'Billing Name: ' . $params['customer']['billing']['first_name'] . ' ' . $params['customer']['billing']['last_name'] . "\n";
   $message .= 'Heard About From: ' . $params['details']['hear_about'] . "\n";
   $message .= 'Phone: ' . $params['customer']['phone'] . "\n";
   $message .= 'Email: ' . $params['customer']['email'] . "\n";
   $message .= 'Company: ' . $params['customer']['billing']['company'] . "\n";
-  $message .= 'Address: ' . $params['customer']['billing']['address'] . "\n";
-  $message .= 'Address Cont.: ' . $params['customer']['billing']['address_2'] . "\n";
+  $message .= 'Address: ' . $params['customer']['billing']['address'] . ' ' . $params['customer']['billing']['address_2'] . "\n";
   $message .= 'City, State, Zip, Country: ' . $params['customer']['billing']['city'] . ', ' . $params['customer']['billing']['state'] . ', ' . $params['customer']['billing']['zip'] . ', ' . $params['customer']['billing']['country'] . "\n";
   $message .= "\n";
   $message .= "==========================================\n";
   $message .= "\n";
   $message .= 'Shipping Name: ' . $params['customer']['shipping']['first_name'] . ' ' . $params['customer']['shipping']['last_name'] . "\n";
   $message .= 'Company: ' . $params['customer']['shipping']['company'] . "\n";
-  $message .= 'Address: ' . $params['customer']['shipping']['address'] . "\n";
-  $message .= 'Address Cont.: ' . $params['customer']['shipping']['address_2'] . "\n";
+  $message .= 'Address: ' . $params['customer']['shipping']['address'] . ' ' . $params['customer']['shipping']['address_2'] . "\n";
   $message .= 'City, State, Zip, Country: ' . $params['customer']['shipping']['city'] . ', ' . $params['customer']['shipping']['state'] . ', ' . $params['customer']['shipping']['zip'] . ', ' . $params['customer']['shipping']['country'] . "\n";
 
   wp_mail($toAddress, $subject, $message);
